0f1d3823c9ccff21355e30b84390c2ac3f0c2a53
Sean Prashad <S.Prashad@outlook.com>
2020-01-02 00:37:06 -0500

    Add react-icons to Intro component

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Container, Row } from 'reactstrap';
+import { Container, Row, NavLink } from 'reactstrap';
+import {
+  FaTwitter,
+  FaLinkedinIn,
+  FaGithub,
+  FaMediumM,
+  FaEnvelope,
+} from 'react-icons/fa';
 
 import './Intro.css';
 
@@ -12,6 +19,24 @@ const Intro = () => {
 
       <Row>
         <p>Programming, Powerlifting && Pizza</p>
+      </Row>
+
+      <Row>
+        <NavLink target="_blank" href="https://bit.ly/seanprashad_github">
+          <FaGithub />
+        </NavLink>
+        <NavLink target="_blank" href="https://bit.ly/seanprashad_linkedin">
+          <FaLinkedinIn />
+        </NavLink>
+        <NavLink target="_blank" href="https://bit.ly/seanprashad_medium">
+          <FaMediumM />
+        </NavLink>
+        <NavLink target="_blank" href="https://bit.ly/seanprashad_twitter">
+          <FaTwitter />
+        </NavLink>
+        <NavLink target="_blank" href="mailto:S.Prashad@outlook.com">
+          <FaEnvelope />
+        </NavLink>
       </Row>
     </Container>
   );
