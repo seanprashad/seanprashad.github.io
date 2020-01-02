b1059317a5a2e1ea8befdadce788602d30baf176
Sean Prashad <S.Prashad@outlook.com>
2020-01-02 00:10:38 -0500

    Adjusting Intro component

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Container, Row } from 'reactstrap';
 
 import './Intro.css';
 
 const Intro = () => {
   return (
-    <div className="Intro">
-      <h1>WELCOME TO MY WORLD!</h1>
-      <p>Programming, Powerlifting && Pizza</p>
-    </div>
+    <Container className="Intro">
+      <Row>
+        <h1>WELCOME TO MY CORNER OF THE NET</h1>
+      </Row>
+
+      <Row>
+        <p>Programming, Powerlifting && Pizza</p>
+      </Row>
+    </Container>
   );
 };
 
