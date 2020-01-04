7c08694eac6308afbea0c4f06487cfed8078d52f
Sean Prashad <S.Prashad@outlook.com>
2020-01-04 18:57:58 -0500

    Refactor About & Education components to remove a redundant class

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -12,34 +12,36 @@ const Education = () => {
         <Row>
           <h2>Education</h2>
         </Row>
-        <Row className="education-content">
-          <Col sm={4}>
-            <Row>
-              <a href="https://bit.ly/seneca_bsd" target="seneca-bsd">
-                <img src={senecaImg} alt="seneca" />
-              </a>
-            </Row>
-            <Row>
-              <h3>Software Development</h3>
-            </Row>
-            <Row>
-              <p>Seneca College, Toronto ON</p>
-            </Row>
-          </Col>
-          <Col sm={4}>
-            <Row>
-              <a href="https://bit.ly/seneca_ect" target="seneca-ect">
-                <img src={senecaImg} alt="seneca" />
-              </a>
-            </Row>
-            <Row>
-              <h3>Computer Engineering</h3>
-            </Row>
-            <Row>
-              <p>Seneca College, Toronto ON</p>
-            </Row>
-          </Col>
-        </Row>
+        <Col>
+          <Row>
+            <Col sm={4}>
+              <Row>
+                <a href="https://bit.ly/seneca_bsd" target="seneca-bsd">
+                  <img src={senecaImg} alt="seneca" />
+                </a>
+              </Row>
+              <Row>
+                <h3>Software Development</h3>
+              </Row>
+              <Row>
+                <p>Seneca College, Toronto ON</p>
+              </Row>
+            </Col>
+            <Col sm={4}>
+              <Row>
+                <a href="https://bit.ly/seneca_ect" target="seneca-ect">
+                  <img src={senecaImg} alt="seneca" />
+                </a>
+              </Row>
+              <Row>
+                <h3>Computer Engineering</h3>
+              </Row>
+              <Row>
+                <p>Seneca College, Toronto ON</p>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
       </Col>
     </Container>
   );
