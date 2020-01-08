d361d64c73dda29190fd56bc89ac841ee55a18aa
Sean Prashad <S.Prashad@outlook.com>
2020-01-08 03:10:49 -0500

    Turn off debugging for Google Analytics

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ import './App.scss';
 
 class App extends React.Component {
   componentDidMount() {
-    initGA('UA-92642042-2', { debug: true });
+    initGA('UA-92642042-2', { debug: false });
     PageView();
   }
 
