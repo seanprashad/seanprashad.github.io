d2182d0687a45bb2a8ff4c586d087881e7ce43f9
Sean Prashad <S.Prashad@outlook.com>
2020-01-08 02:03:33 -0500

    Add events for Contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -19,7 +19,13 @@ const Contact = () => {
         <Col>
           <Row className="social">
             <Col sm={1}>
-              <NavLink target="_blank" href="https://bit.ly/seanprashad_github">
+              <NavLink
+                target="_blank"
+                href="https://bit.ly/seanprashad_github"
+                onClick={() =>
+                  Event('Contact', 'Clicked a social link', 'GitHub link')
+                }
+              >
                 <FaGithub />
               </NavLink>
             </Col>
@@ -27,17 +33,32 @@ const Contact = () => {
               <NavLink
                 target="_blank"
                 href="https://bit.ly/seanprashad_linkedin"
+                onClick={() =>
+                  Event('Contact', 'Clicked a social link', 'LinkedIn link')
+                }
               >
                 <FaLinkedin />
               </NavLink>
             </Col>
             <Col sm={1}>
-              <NavLink target="_blank" href="https://bit.ly/seanprashad_medium">
+              <NavLink
+                target="_blank"
+                href="https://bit.ly/seanprashad_medium"
+                onClick={() =>
+                  Event('Contact', 'Clicked a social link', 'Medium link')
+                }
+              >
                 <FaMedium />
               </NavLink>
             </Col>
             <Col sm={1}>
-              <NavLink target="_blank" href="mailto:S.Prashad@outlook.com">
+              <NavLink
+                target="_blank"
+                href="mailto:S.Prashad@outlook.com"
+                onClick={() =>
+                  Event('Contact', 'Clicked a social link', 'Email link')
+                }
+              >
                 <FaEnvelope />
               </NavLink>
             </Col>
