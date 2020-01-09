b54fc04ba347f7ad9565e004728335ffb1cd8d98
Sean Prashad <S.Prashad@outlook.com>
2020-01-09 14:36:27 -0500

    Shrink Hackathon section images to prevent Parallax bugs

--- src/components/Hackathon/index.js
+++ src/components/Hackathon/index.js
@@ -31,50 +31,48 @@ const Hackathon = () => {
             <h2>Hackathons</h2>
           </Row>
           <Col>
-            <Col>
-              <Row>
-                <Col sm={3}>
-                  <h4>
-                    <span className="winna">4th Place</span> - Genesys Hackathon
-                  </h4>
-                  <img src={genesysImg} alt="genesys" />
-                </Col>
-              </Row>
+            <Row>
+              <Col sm={2}>
+                <h4>
+                  <span className="winna">4th Place</span> - Genesys Hackathon
+                </h4>
+                <img src={genesysImg} alt="genesys" />
+              </Col>
+            </Row>
 
-              <Row>
-                <Col sm={3}>
-                  <h4>
-                    <span className="winna">2nd Place</span> - HackingGood 2017
-                  </h4>
-                  <img src={hackingGoodImg} alt="hackingGood" />
-                </Col>
-                <Col sm={3}>
-                  <h4>Mentor & Judge - TOHacks 2018</h4>
-                  <img src={tohacksImg} alt="toHacks" />
-                </Col>
-                <Col sm={3}>
-                  <h4>RBC Next Great Innovator</h4>
-                  <img src={rbcNGIImg} alt="rbcNGI" />
-                </Col>
-              </Row>
+            <Row>
+              <Col sm={2}>
+                <h4>
+                  <span className="winna">2nd Place</span> - HackingGood 2017
+                </h4>
+                <img src={hackingGoodImg} alt="hackingGood" />
+              </Col>
+              <Col sm={2}>
+                <h4>Mentor & Judge - TOHacks 2018</h4>
+                <img src={tohacksImg} alt="toHacks" />
+              </Col>
+              <Col sm={2}>
+                <h4>RBC Next Great Innovator</h4>
+                <img src={rbcNGIImg} alt="rbcNGI" />
+              </Col>
+            </Row>
 
-              <Row>
-                <Col sm={3}>
-                  <h4>OpenIDEO 2017</h4>
-                  <img src={openIDEOImg} alt="openIDEO" />
-                </Col>
-                <Col sm={3}>
-                  <h4>GM HackAI</h4>
-                  <img src={gmHackAIImg} alt="gmHackAI" />
-                </Col>
-                <Col sm={3}>
-                  <h4>
-                    <span className="winna">3rd Place</span> - OSCE Days 2017
-                  </h4>
-                  <img src={osceDaysImg} alt="osceDays" />
-                </Col>
-              </Row>
-            </Col>
+            <Row>
+              <Col sm={2}>
+                <h4>OpenIDEO 2017</h4>
+                <img src={openIDEOImg} alt="openIDEO" />
+              </Col>
+              <Col sm={2}>
+                <h4>GM HackAI</h4>
+                <img src={gmHackAIImg} alt="gmHackAI" />
+              </Col>
+              <Col sm={2}>
+                <h4>
+                  <span className="winna">3rd Place</span> - OSCE Days 2017
+                </h4>
+                <img src={osceDaysImg} alt="osceDays" />
+              </Col>
+            </Row>
           </Col>
         </Col>
       </Container>
