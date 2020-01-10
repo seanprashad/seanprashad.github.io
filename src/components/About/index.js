82e16a8693db428b6a243ce2ecd211df30f725c0
Sean Prashad <S.Prashad@outlook.com>
2020-01-10 17:45:09 -0500

    Use font-awesome icons for About section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Container, Col, Row } from 'reactstrap';
+import { IconContext } from 'react-icons';
 import { Link } from 'react-scroll';
-import DevIcon from 'devicon-react-svg';
+import { FaDocker, FaGithub, FaJava, FaReact } from 'react-icons/fa';
+import { IoLogoJavascript } from 'react-icons/io';
+import { DiMysql } from 'react-icons/di';
 import { Event } from '../Shared/Tracking';
 
 import aboutImg from '../../images/about.jpg';
@@ -20,79 +23,82 @@ const About = () => {
             <Col sm={2}>
               <img src={aboutImg} alt="profile" />
             </Col>
-            <Col sm={3}>
+            <Col sm={4}>
               <Row>
-                <p>
-                  Hey! I&apos;m <strong>Sean</strong>, a{' '}
-                  <strong>Software Development</strong> and{' '}
-                  <strong>Computer Engineering</strong> graduate from Seneca.
-                </p>
+                <Row>
+                  <p>
+                    Hey! I&apos;m <strong>Sean</strong>, a{' '}
+                    <strong>Software Development</strong> and{' '}
+                    <strong>Computer Engineering</strong> graduate from Seneca.
+                  </p>
+                </Row>
+                <Row>
+                  <p>
+                    In my spare time, you can usually find me{' '}
+                    <a
+                      href="http://bit.ly/secret_sean"
+                      target="instagram"
+                      onClick={() =>
+                        Event(
+                          'About Me',
+                          'Clicked Instagram link',
+                          'Instagram link',
+                        )
+                      }
+                    >
+                      <strong>training</strong>
+                    </a>
+                    , helping in the{' '}
+                    <a
+                      href="http://bit.ly/github_prs"
+                      target="github"
+                      onClick={() =>
+                        Event(
+                          'About Me',
+                          "Clicked GitHub PR's link",
+                          "GitHub PR's link",
+                        )
+                      }
+                    >
+                      <strong>OSS community</strong>
+                    </a>
+                    , and letting my imagination run wild at{' '}
+                    <Link
+                      to="hackathon"
+                      activeClass="active"
+                      spy
+                      smooth
+                      duration={1000}
+                      onClick={() =>
+                        Event(
+                          'About Me',
+                          'Clicked Hackathons link',
+                          'Hackathons link',
+                        )
+                      }
+                    >
+                      <strong>hackathons</strong>
+                    </Link>
+                    .
+                  </p>
+                </Row>
+                <Row>
+                  <p>
+                    I&apos;m always tinkering with new technologies but here are
+                    some of the past ones that I&apos;ve worked with:
+                  </p>
+                </Row>
               </Row>
               <Row>
-                <p>
-                  In my spare time, you can usually find me{' '}
-                  <a
-                    href="http://bit.ly/secret_sean"
-                    target="instagram"
-                    onClick={() =>
-                      Event(
-                        'About Me',
-                        'Clicked Instagram link',
-                        'Instagram link',
-                      )
-                    }
-                  >
-                    <strong>training</strong>
-                  </a>
-                  , helping in the{' '}
-                  <a
-                    href="http://bit.ly/github_prs"
-                    target="github"
-                    onClick={() =>
-                      Event(
-                        'About Me',
-                        "Clicked GitHub PR's link",
-                        "GitHub PR's link",
-                      )
-                    }
-                  >
-                    <strong>OSS community</strong>
-                  </a>
-                  , and letting my imagination run wild at{' '}
-                  <Link
-                    to="hackathon"
-                    activeClass="active"
-                    spy
-                    smooth
-                    duration={1000}
-                    onClick={() =>
-                      Event(
-                        'About Me',
-                        'Clicked Hackathons link',
-                        'Hackathons link',
-                      )
-                    }
-                  >
-                    <strong>Hackathons</strong>
-                  </Link>
-                  .
-                </p>
+                <IconContext.Provider value={{ size: '5em' }}>
+                  <FaJava />
+                  <IoLogoJavascript />
+                  <FaReact />
+                  <FaGithub />
+                  <FaDocker />
+                  <DiMysql />
+                </IconContext.Provider>
               </Row>
-              <Row>
-                <p>
-                  I&apos;m always tinkering with new technologies but here are
-                  some of the past ones that I&apos;ve worked with:
-                </p>
-              </Row>
-            </Col>
-            <Col sm={2}>
-              <DevIcon viewBox="0 0 32 32" icon="java" />
-              <DevIcon viewBox="0 0 32 32" icon="javascript" />
-              <DevIcon viewBox="0 0 32 32" icon="go" />
-              <DevIcon viewBox="0 0 32 32" icon="react" />
-              <DevIcon viewBox="0 0 32 32" icon="github_badge" />
-              <DevIcon viewBox="0 0 32 32" icon="docker" />
-              <DevIcon viewBox="0 0 32 32" icon="mysql" />
             </Col>
           </Row>
         </Col>
