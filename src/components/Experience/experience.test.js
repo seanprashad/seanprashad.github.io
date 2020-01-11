461d635efe44cf1ee7cd3e666aa2b1be0befc7eb
Sean Prashad <S.Prashad@outlook.com>
2020-01-10 19:59:59 -0500

    Add unit tests for Experience component

---
+++ src/components/Experience/experience.test.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import Experience from '.';
+
+it('matches the snapshot', () => {
+  const tree = renderer.create(<Experience />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders the Experience component without crashing', () => {
+  shallow(<Experience />);
+});
+
+it('renders the main header', () => {
+  const wrapper = shallow(<Experience />);
+
+  expect(wrapper.find('h2').html()).toEqual('<h2>Experience</h2>');
+});
+
+it('renders details for PagerDuty', () => {
+  const wrapper = shallow(<Experience />);
+
+  expect(
+    wrapper
+      .find('Col>Row>Col')
+      .at(0)
+      .html(),
+  ).toEqual(
+    '<div class="col-sm-3"><div class="row"><a href="https://bit.ly/pagerduty_home" target="pagerduty"><img src="pagerduty.png" alt="pagerduty"/></a></div><div class="row"><h3>Software Engineer Intern</h3></div><div class="row"><h4>PagerDuty, Toronto ON</h4></div><div class="row"><p>September 2019 - December 2019</p></div></div>',
+  );
+});
+
+it('renders details for Wattpad', () => {
+  const wrapper = shallow(<Experience />);
+
+  expect(
+    wrapper
+      .find('Col>Row>Col')
+      .at(1)
+      .html(),
+  ).toEqual(
+    '<div class="col-sm-3"><div class="row"><a href="https://bit.ly/wattpad_home" target="wattpad"><img src="wattpad.png" alt="wattpad"/></a></div><div class="row"><h3>Software Engineer Intern</h3></div><div class="row"><h4>Wattpad, Toronto ON</h4></div><div class="row"><p>January 2019 - April 2019</p></div></div>',
+  );
+});
+
+it('renders details for Capital One', () => {
+  const wrapper = shallow(<Experience />);
+
+  expect(
+    wrapper
+      .find('Col>Row>Col')
+      .at(2)
+      .html(),
+  ).toEqual(
+    '<div class="col-sm-3"><div class="row"><a href="https://bit.ly/capitalone_home" target="capital-one"><img src="capital-one.png" alt="capital-one"/></a></div><div class="row"><h3>Software Engineer Intern</h3></div><div class="row"><h4>Capital One, Toronto ON</h4></div><div class="row"><p>May 2018 - August 2018</p></div></div>',
+  );
+});
+
+it('renders details for Seneca', () => {
+  const wrapper = shallow(<Experience />);
+
+  expect(
+    wrapper
+      .find('Col>Row>Col')
+      .at(3)
+      .html(),
+  ).toEqual(
+    '<div class="col-sm-3"><div class="row"><a href="https://bit.ly/seneca_home" target="seneca"><img src="seneca.png" alt="seneca"/></a></div><div class="row"><h3>Tutor</h3></div><div class="row"><h4>Seneca, Toronto ON</h4></div><div class="row"><p>September 2017 - April 2018</p></div></div>',
+  );
+});
+
+it('renders details for BMO', () => {
+  const wrapper = shallow(<Experience />);
+
+  expect(
+    wrapper
+      .find('Col>Row>Col')
+      .at(4)
+      .html(),
+  ).toEqual(
+    '<div class="col-sm-3"><div class="row"><a href="https://bit.ly/bmo_home" target="bmo"><img src="bmo.png" alt="bmo"/></a></div><div class="row"><h3>Software Engineer Intern</h3></div><div class="row"><h4>Bank of Montreal, Toronto ON</h4></div><div class="row"><p>May 2017 - September 2017</p></div></div>',
+  );
+});
+
+it('renders details for Vincent Associates', () => {
+  const wrapper = shallow(<Experience />);
+
+  expect(
+    wrapper
+      .find('Col>Row>Col')
+      .at(5)
+      .html(),
+  ).toEqual(
+    '<div class="col-sm-3"><div class="row"><a href="https://bit.ly/vincentinc_home" target="vincent-associates"><img src="vincent-associates.png" alt="vincent-associates"/></a></div><div class="row"><h3>Software Engineer</h3></div><div class="row"><h4>Vincent Associates, Toronto ON</h4></div><div class="row"><p>May 2015 - August 2016</p></div></div>',
+  );
+});
